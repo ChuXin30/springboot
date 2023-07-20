e23493c5375d5a86af6bd1829d1ad7c2f5e4b185
haochen <957652322@qq.com>
2023-07-20 23:47:25 +0800

    限流

---
+++ src/main/java/com/example/springboot1/Aspect/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.springboot1.Aspect;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author yzd
+ * @Description: 注入自定义拦截器
+ * @create 2021-05-07 14:03
+ **/
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AccessLimitInterceptor accessLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(accessLimitInterceptor);
+    }
+}
