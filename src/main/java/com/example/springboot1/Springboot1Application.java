e8ae24ec03fdc911cec0fefa9b3f93c64daffc92
haochen <957652322@qq.com>
2023-07-23 19:33:21 +0800

    1

--- src/main/java/com/example/springboot1/Springboot1Application.java
+++ src/main/java/com/example/springboot1/Springboot1Application.java
@@ -25,6 +25,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.example.springboot1.mapper")
 public class Springboot1Application {
 
+
     public static void main(String[] args) {
         //以为是启动了一个方法，没想到启动了一个服务
         SpringApplication.run(Springboot1Application.class, args);
