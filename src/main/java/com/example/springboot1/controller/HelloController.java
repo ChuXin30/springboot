6d48d240aa98fdeb643b5d53d6ca8c8dea9be543
haochen <957652322@qq.com>
2024-07-10 22:22:06 +0800

    设计模式：建造者模式04

--- src/main/java/com/example/springboot1/controller/HelloController.java
+++ src/main/java/com/example/springboot1/controller/HelloController.java
@@ -25,4 +25,34 @@ public class HelloController {
         return "Hello World";
     }
 
+    //给我实现一个快速排序算法
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int i = low, j = high, temp = arr[low];
+            while (i < j) {
+                while (i < j && arr[j] >= temp) {
+                    j--;
+                }
+                if (i < j) {
+                    arr[i] = arr[j];
+                    i++;
+                }
+                while (i < j && arr[i] < temp) {
+                    i++;
+                }
+                if (i <j) {
+                }
+            }
+        }
+
+        arr[i] = temp;
+        quickSort(arr, low, i - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        quickSort(arr, 0, arr.length - 1);
+        for (   )
+    }
+
 }
